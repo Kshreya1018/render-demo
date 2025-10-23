b26f865376f1d8622381eafe149f57ca9a3ab133
Kshreya1018 <shreyasahu1003@gmail.com>
2025-10-23 11:14:35 +0530

    Fix formatting and indentation in app.py

--- app.py
+++ app.py
@@ -1,42 +1,28 @@
-from flask import Flask, jsonify,request, render_template
-
+from flask import Flask, jsonify, request, render_template
 import pickle
-
 import numpy as np
 
-model_path='best_model.pkl'  
+model_path = 'best_model.pkl'
 
 with open(model_path, 'rb') as file:
-
     modell = pickle.load(file)
 
 app = Flask(__name__)
 
 @app.route('/')
-
 def home():
-
     return render_template('index.html')
 
 @app.route('/predict', methods=['POST'])
-
 def predict():
-
     try:
-
         features = [float(x) for x in request.form.values()]
-
         final_features = [np.array(features)]
-
         prediction = modell.predict(final_features)
-
         output = round(prediction[0], 2)
-
         return render_template('index.html', prediction_text='Predicted Value: {}'.format(output))
-
     except Exception as e:
-
         return render_template('index.html', prediction_text='Error: {}'.format(str(e)))
-    
-    if __name__ == '__main__':
-        app.run(debug=True)
+
+if __name__ == '__main__':
+    app.run(debug=True)
